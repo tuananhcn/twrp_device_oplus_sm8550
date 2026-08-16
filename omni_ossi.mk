f29dc52700c0894743bdfa5ac447de43c4e5c5d9
tuananhcn <99953080+tuananhcn@users.noreply.github.com>
2026-08-17 00:59:53 +0700

    Update omni_ossi.mk

--- omni_ossi.mk
+++ omni_ossi.mk
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/oplus/ossi
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
